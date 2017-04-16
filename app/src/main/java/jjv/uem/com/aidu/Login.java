b2d0c41e97df2fec48f3a1c922bf7fb0f5651047
Javi M. Lizama <jfmarli@gmail.com>
2017-04-16 12:48:47 +0200

    Login 1.4 - sign in with email/password done

--- app/src/main/java/jjv/uem/com/aidu/Login.java
+++ app/src/main/java/jjv/uem/com/aidu/Login.java
@@ -1,17 +1,29 @@
 package jjv.uem.com.aidu;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private static final String ERROR_PWD_INCORRECTA = "com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The password is invalid or the user does not have a password.";
+    private static final String ERROR_MAIL_NO_EXISTENTE = "com.google.firebase.auth.FirebaseAuthInvalidUserException: There is no user record corresponding to this identifier. The user may have been deleted.";
+    private static final String ERROR_USR_INCORRECTO = "com.google.firebase.auth.FirebaseAuthInvalidUserException: There is no user record corresponding to this identifier. The user may have been deleted.";
+    private static final String ERROR_FORMATO = "com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The email address is badly formatted.";
 
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authStateListener;
@@ -62,8 +74,47 @@ public class Login extends AppCompatActivity {
         };
     }
 
-    private void login(String user, String pwd){
+    public void login(View v){
+        String user = etUserName.getText().toString(), pwd = etPwd.getText().toString();
+        if(user.equals("") || pwd.equals("")){
+            Toast.makeText(this, getText(R.string.login_toast_creden), Toast.LENGTH_SHORT).show();
+        } else {
+            auth.signInWithEmailAndPassword(user, pwd)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("LOGIN-COMPLETE: ", "" + task.isSuccessful());
 
+                            if (!task.isSuccessful()) {
+                                if (task.getException().toString().equals(ERROR_USR_INCORRECTO)){
+                                    //Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                                    Toast.makeText(getBaseContext(), R.string.incorrect_email,
+                                            Toast.LENGTH_SHORT).show();
+                                }else if (task.getException().toString().equals(ERROR_MAIL_NO_EXISTENTE)){
+                                    //Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                                    Toast.makeText(getBaseContext(), R.string.incorrect_email,
+                                            Toast.LENGTH_SHORT).show();
+                                } else if (task.getException().toString().equals(ERROR_PWD_INCORRECTA)){
+                                    //Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                                    Toast.makeText(getBaseContext(), R.string.incorrect_pwd,
+                                            Toast.LENGTH_SHORT).show();
+                                }else if (task.getException().toString().equals(ERROR_FORMATO)){
+                                    //Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                                    Toast.makeText(getBaseContext(), R.string.wrong_mail_format,
+                                            Toast.LENGTH_SHORT).show();
+                                }else{
+                                    //Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                                    Toast.makeText(getBaseContext(), R.string.auth_failed,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                // si all es correcto inicia MainActivity
+                            }else{
+                                Intent i = new Intent(Login.this, MainActivity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+        }
     }
 
     //asigna el listener a la instancia de auth
