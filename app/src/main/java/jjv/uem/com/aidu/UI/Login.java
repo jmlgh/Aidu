cf2bfada7d248de177e70dc6bf5a12a708b89e97
javiml <jfmarli@gmail.com>
2017-05-01 00:48:12 +0200

    Google sign in done Animation Login-Main and Login-Register done Background color change in Main Navigation drawer icon, color and text change (From javim)

--- app/src/main/java/jjv/uem/com/aidu/UI/Login.java
+++ app/src/main/java/jjv/uem/com/aidu/UI/Login.java
@@ -9,29 +9,43 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 import jjv.uem.com.aidu.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String ERROR_PWD_INCORRECTA = "com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The password is invalid or the user does not have a password.";
     private static final String ERROR_MAIL_NO_EXISTENTE = "com.google.firebase.auth.FirebaseAuthInvalidUserException: There is no user record corresponding to this identifier. The user may have been deleted.";
     private static final String ERROR_USR_INCORRECTO = "com.google.firebase.auth.FirebaseAuthInvalidUserException: There is no user record corresponding to this identifier. The user may have been deleted.";
     private static final String ERROR_FORMATO = "com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The email address is badly formatted.";
+    private static final int RC_SIGN_IN = 9001;
+
 
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authStateListener;
-    private TextView tvLogo, tvRegister;
+    private TextView tvLogo, tvRegister, tvAltLogin;
     private EditText etUserName, etPwd;
+    private ImageView ivGoogleSignInButton;
     private Button btnLogin;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,20 @@ public class Login extends AppCompatActivity {
         initViews();
         setTypeFace();
         initFirebase();
+        initGoogleSignIn();
+    }
+
+    private void initGoogleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* Fragment Activity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
     private void initViews() {
@@ -48,12 +76,26 @@ public class Login extends AppCompatActivity {
         etPwd = (EditText) findViewById(R.id.et_password);
         tvLogo = (TextView) findViewById(R.id.r_tv_logo);
         tvRegister = (TextView) findViewById(R.id.tv_register);
+        tvAltLogin = (TextView) findViewById(R.id.tv_signinalt);
+        ivGoogleSignInButton = (ImageView) findViewById(R.id.iv_google_signin_button);
+
+        ivGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.iv_google_signin_button:
+                        signInWithGoogle();
+                        break;
+                }
+            }
+        });
 
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Login.this, Register.class);
                 startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
             }
         });
 
@@ -67,6 +109,7 @@ public class Login extends AppCompatActivity {
         etUserName.setTypeface(bubblerFont);
         etPwd.setTypeface(bubblerFont);
         btnLogin.setTypeface(bubblerFont);
+        tvAltLogin.setTypeface(bubblerFont);
     }
 
     private void initFirebase() {
@@ -84,6 +127,11 @@ public class Login extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    private void signInWithGoogle(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     public void login(View v){
@@ -123,10 +171,29 @@ public class Login extends AppCompatActivity {
                             }else{
                                 Intent i = new Intent(Login.this, MainActivity.class);
                                 startActivity(i);
+                                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
                             }
                         }
                     });
         }
+    }
+
+    private void loginWithGoogleAccount(GoogleSignInAccount googleAccount) {
+        // consigue un token que configure una credencial de la cuenta de google para la cuenta firebase
+        AuthCredential credential = GoogleAuthProvider.getCredential(googleAccount.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent i = new Intent(Login.this, MainActivity.class);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     //asigna el listener a la instancia de auth
@@ -144,4 +211,27 @@ public class Login extends AppCompatActivity {
             auth.removeAuthStateListener(authStateListener);
         }
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("GOOGLE_L:" , "Status: " + requestCode);
+        // Resultado devuelto de GoogleSignInApi.getSignInIntent(...)
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult loginResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            int statusCode = loginResult.getStatus().getStatusCode();
+
+            if(loginResult.isSuccess()){
+                // GoogleSignIn satisfactorio, inicial el Login con Firebase
+                GoogleSignInAccount googleAccount = loginResult.getSignInAccount();
+                loginWithGoogleAccount(googleAccount);
+            }
+        }
+    }
+
 }
