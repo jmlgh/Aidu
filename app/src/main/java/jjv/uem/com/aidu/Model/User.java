580405644ca433f8088965b90bb7d2f1d59e6d4b
MunozVictor <viketore18@hotmail.com>
2017-06-11 17:21:05 +0200

    merge 3 ramas

--- app/src/main/java/jjv/uem/com/aidu/Model/User.java
+++ app/src/main/java/jjv/uem/com/aidu/Model/User.java
@@ -32,7 +32,7 @@ public class User {
 
     }
 
-    public User(){};
+
 
     public int getPoints() {
         return points;
@@ -40,54 +40,6 @@ public class User {
 
     public void setPoints(int points) {
         this.points = points;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public double getNumVal() {
-        return numVal;
-    }
-
-    public void setNumVal(double numVal) {
-        this.numVal = numVal;
-    }
-
-    public double getMedia() {
-        return media;
-    }
-
-    public void setMedia(double media) {
-        this.media = media;
     }
 
     public String getEmail() {
@@ -155,19 +107,5 @@ public class User {
         this.points+=i;
     }
 
-    /*
-    |-NOMBRE
-|-EMAIL
-|-VALORACIÓN MEDIA
-|-SALDO DE PUNTOS
-|-UBICACIÓN HABITUAL
-|-FOTO PERFIL
-|-LISTA VALORACIONES - PUNTUACIÓN
-			|-------VALORACIÓN (COMENTARIO)
-			|-------USUARIO QUE HA REALIZADO - NOMBRE Y KEY
-|-COMUNIDADES
-|-KEY COMUNIDAD
-|-USUARIOS FAVS
-|-KEY USUARIO
-     */
+
 }
